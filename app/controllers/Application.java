5a39936bd3b546987f18ab7ca97682e917e4c30d
ozan.can.altiok <ozancanaltiok@ozancanaltiok-HPE-500tr.(none)>
2014-06-30 16:20:34 +0300

    Main menu modified, video uploader therotically put.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,6 +2,7 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import play.mvc.Http.MultipartFormData.FilePart;
 
 import play.data.DynamicForm;
 import play.data.Form;
@@ -11,6 +12,9 @@ import views.html.*;
 
 import models.*;
 
+import java.util.HashMap;
+import java.io.File;
+
 public class Application extends Controller {
 
     public static Result index() {
@@ -19,7 +23,7 @@ public class Application extends Controller {
     
     public static Result login() {
 		if ( session().containsKey( "nickName") ) {
-			return redirect( routes.Application.mainMenu() );
+			return redirect( routes.Application.mainMenu( null) );
 		}
 		else {
 			return ok( login.render( form( UserLogin.class) ) );
@@ -31,11 +35,10 @@ public class Application extends Controller {
 		UserLogin op = new UserLogin();
 		op.nick = loginForm.field( "nick").value();
 		op.pwd = loginForm.field( "pwd").value();
-		System.out.println(  );
 		if ( op.validate() == null ) {
 			session().clear();
 			session().put( "nickName", loginForm.field( "nick").value() );
-			return redirect( routes.Application.mainMenu() );
+			return redirect( routes.Application.mainMenu( null) );
 		}
 		else if ( loginForm.hasErrors() ) {
 			return badRequest( login.render( loginForm) );
@@ -43,17 +46,89 @@ public class Application extends Controller {
 		else {
 			session().clear();
 			session().put( "nickName", loginForm.field( "nick").value() );
-			return redirect( routes.Application.mainMenu() );
+			return redirect( routes.Application.mainMenu( null) );
 		}
 	}
 	
+	public static Result watchVideo( String id) {
+		return redirect( routes.Application.mainMenu( null) );
+	}
+	
+	public static Result search() {
+		return redirect( routes.Application.mainMenu( null) );
+	}
+	
+	/*
 	public static Result mainMenu() {
-		return ok( mainMenu.render() );
+		if ( session().containsKey( "nickName") ) {
+			User fetched = User.fetchUser( session().get( "nickName") );
+			return ok( mainMenu.render( fetched, Video.all( fetched), form( VideoUpload.class), new HashMap<String, String>() ) );
+		}
+		else {
+			return redirect( routes.Application.login() );
+		}
+	}
+	*/
+	
+	public static Result mainMenu( String error) {
+		if ( session().containsKey( "nickName") ) {
+			User fetched = User.fetchUser( session().get( "nickName") );
+			HashMap<String, String> messages = new HashMap<String, String>();
+			String[] errors = { "Video is successfully uploaded!", "Error saving content, please contact the admin.", "The file is not available!"};
+			if ( error != null) {
+				if ( error.substring( 0,5).equals( "error") ) {
+					if ( error.length() == 6) {
+						messages.put( "uploadError", errors[ Integer.parseInt( error.substring(5) ) - 1 ]);
+					}
+				}
+			}
+			return ok( mainMenu.render( fetched, Video.all( fetched), form( VideoUpload.class), messages ) );
+		}
+		else {
+			return redirect( routes.Application.login() );
+		}
+	}
+	
+	public static Result addVideo() {
+		if ( session().containsKey( "nickName") ) {
+			Form<VideoUpload> uploadForm = form( VideoUpload.class).bindFromRequest();
+			FilePart videoFile = request().body().asMultipartFormData().getFile( "videoFile");
+			
+			if ( videoFile != null) {
+				User fetched = User.fetchUser( session().get( "nickName") );
+				String videoSaved = Video.create( uploadForm.field( "header").value(), fetched );
+				
+				if ( videoSaved != null) {
+					String fileName = videoFile.getFilename();
+					String extension = "";
+					
+					int x = fileName.length() - 1;
+					while ( fileName.charAt(x) != '.' ) {
+						extension = fileName.charAt(x) + extension;
+						x--;
+					}
+					
+					File finalUpload = videoFile.getFile();
+					String uploadPath = Play.application().configuration().getString( "upload.path", "/tmp/");
+					finalUpload.renameTo( new File( uploadPath + videoSaved + "." + extension ) );
+					return redirect( routes.Application.mainMenu( "error1") );
+				}
+				else {
+					return redirect( routes.Application.mainMenu( "error2") );
+				}
+			}
+			else {
+				return redirect( routes.Application.mainMenu( "error3") );
+			}
+		}
+		else {
+			return redirect( routes.Application.login() );
+		}
 	}
 	
 	public static Result registerMenu() {
 		if ( session().containsKey( "nickName") ) {
-			return redirect( routes.Application.mainMenu() );
+			return redirect( routes.Application.mainMenu( null) );
 		}
 		else {
 			return ok( registrationForm.render( form( UserRegistration.class) ) );
@@ -103,6 +178,10 @@ public class Application extends Controller {
 				}
 			}
 		}
+	}
+	
+	public static class VideoUpload {
+		public String header;
 	}
 
 }
